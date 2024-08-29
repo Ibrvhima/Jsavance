d1d7324ba5c1e5b2e03a98d8f4882abfdc544fc8
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-08-29 09:39:29 +0000

    my first commit

---
+++ reponse.md
@@ -0,0 +1,27 @@
+### Question 1
+Qu'est-ce qu'un objet en JavaScript ?
+
+REP: En java script les objets sont des collection d'une paire de clé et de valeur.
+
+
+### Question 2
+Quelle est la différence entre la notation par point et la notation par crochets pour accéder aux propriétés d'un objet ?
+
+REP: La difference est:
+* la notation par point nous permet d'acceder aux proprietés d'un objet plus facilement et plus direct.
+tant dis que la notation  par crochets et plus flexible.
+
+
+### Question 3
+Comment parcourez-vous les propriétés d'un objet en JavaScript ?
+
+REP En java script pour parcourir les proprietés d'un objet on utilsie la boucle "for" ou "for in"
+
+
+### Question 4
+Quelle est la différence entre un objet et un tableau en JavaScript ?
+
+
+REP: un objet est un ensemble de clé et de valeurs
+tant dis qu'un tableau est une structure de données fondanmental qui nous permet de stocker et de manupiler efficacement un ensemble de collection.
+
---
+++ script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+//### Question 5
+//Écrivez une fonction JavaScript pour convertir un objet en une liste de paires [clé, valeur].//
+
+const person = {
+  hisName: "Diallo",
+  hisDjob: "Student",
+};
+function convertToListKeyValues() {
+  return Object.entries(person);
+}
+
+console.log(convertToListKeyValues());
+
+//### Question 6
+//Écrivez une fonction qui prend un objet représentant une personne et retourne son nom complet.//
+
+function aPerson(objet) {
+  return console.log({ firstName: "diallo", lastName: "ibrahima" });
+}
+aPerson();
+
+//### Question 7
+//Créez un objet avec vos détails personnels. Imprimez maintenant toutes les clés de l'objet dans l'ordre croissant.
+
+const Me = {
+  myName: "diallo",
+  myAge: 20,
+  myHeight: 178,
+  job: "student",
+};
+console.log(Object.keys(Me));
+
+//## Question 8
+//Créez un objet avec vos détails personnels. Filtrez maintenant toutes les valeurs de l'objet et affichez-les dans l'ordre décroissant.
+const me = {
+  myName: "diallo",
+  myAge: 20,
+  myHeight: 178,
+  job: "student",
+};
+
+console.log(Object.values(Me));
+
+//## Question 9
+
+//Créez un objet pour contenir des informations sur votre recette préférée. Il doit avoir des propriétés pour le titre (une chaîne de caractères), les portions (un nombre) et les ingrédients (un tableau de chaînes de caractères). Sur des lignes séparées (une instruction `console.log` pour chaque), affichez les informations de la recette de sorte qu'elles ressemblent à ceci :
+
+const myFavoriteFood = {
+  title: "Salad",
+  portion: 2,
+  ingredients: ["salades", "tomatos", "salt", "eggs"],
+};
+console.log(myFavoriteFood.title);
+console.log(myFavoriteFood.portion);
+console.log("ingredient :");
+myFavoriteFood.ingredients.forEach((element) => {
+  console.log(element);
+});
+
+//### Question 10
+
+//Créez une fonction JavaScript à l'intérieur d'un objet qui trouve le maximum de 3 nombres. Appelez maintenant cette fonction de l'objet et imprimez le nombre maximum.
+
+const findMaiximumNumber = {
+  maxNumber: function (number1, number2, number3) {
+    return Math.max(number1, number2, number3);
+  },
+};
+const maximum = findMaiximumNumber.maxNumber(1, 2, 3)
+console.log(`le maximum est : ${maximum}`);
